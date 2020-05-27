925a570a67208e4b901e93ee164c776b9a95a659
Kilhyun Kim <kid1064@gmail.com>
2020-05-27 19:23:58 +0900

    [R Bacic] 1.About R

---
+++ [R Basic]1. R object.r
@@ -0,0 +1,45 @@
+# 숫자형
+a <- 3
+b <- 4.5
+
+print(a);print(b)
+
+# 사칙 연산
+a <- 1
+b <- 2
+c <- 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(c %/% b) # 몫
+print(c %% b)  # 나머지
+print(b ** c)  # 거듭제곱
+
+
+# 문자열
+str1 <- "hello"
+str2 <- 'world'
+
+print(str1);print(str2)
+
+# 불리언
+bool1 <- T
+bool2 <- TRUE
+bool3 <- F
+bool4 <- FALSE
+
+print(bool1); print(bool2)
+print(bool3); print(bool4)
+
+print(bool1 & bool2)
+print(bool2 | bool3)
+print(!bool4)
+
+# NA
+var_na <- NA
+is.na(var_na)
+
+var_null <- NULL
+is.null(var_null)
